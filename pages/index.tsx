cf647f9df21c7bc65bff207d355b027dbb285e4e
Ilia Skliaruk <iliaskliaruk@Ilia-MacBook-Air.local>
2022-08-30 21:25:18 +0300

    Define some routes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 
+import { query } from 'firebase/firestore'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
-    
+      <Link prefetch={false} href={{
+        pathname: '/[username]',
+        query: {username: 'iliaskl'}
+      }}> </Link>
     </div>
   )
 }
